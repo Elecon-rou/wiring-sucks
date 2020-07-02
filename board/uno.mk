d86d9c75d4b48838ad33e7f25255e2cfdb2823e2
Elecon-rou <elecon@xecut.net>
2020-07-02 07:39:39 +0000

    add multiple pin definitions

--- board/uno.mk
+++ board/uno.mk
@@ -1,3 +1,3 @@
-CFLAGS=-Os -mmcu=atmega328p -DF_CPU=16000000L 
+CFLAGS=-Os -mmcu=atmega328p -DF_CPU=16000000L -I../../include/Core/pins_uno -Iinclude/Core/pins_uno
 CXXFLAGS=$(CFLAGS)
 PROGFLAGS=-p m328p -c arduino
